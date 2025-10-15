4bef08ed32a4225ffed707e3303fa03f9527d426
Justin Liao <b11902161@ntu.edu.tw>
2025-10-16 01:44:11 +0800

    Initial Commit

---
+++ Socket Programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Socket Programming
+//
+//  Created by Justin Liao on 2025.10.16.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
